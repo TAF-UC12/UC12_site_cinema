6eca19709a9545aa14290f0a0f9c8fedc7bba585
thiago-z <37704790+thiago-z@users.noreply.github.com>
2018-04-16 17:38:48 -0300

    Alteração filme.php

--- filme.php
+++ filme.php
@@ -28,61 +28,7 @@
     <script src="js/jquery-3.3.1.js" type="text/javascript"></script>
     <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
     <script src="js/jssor.slider-27.1.0.min.js" type="text/javascript"></script>
-    <script type="text/javascript">
-        jssor_1_slider_init = function() {
-
-            var jssor_1_SlideoTransitions = [
-              [{b:-1,d:1,o:-0.7}],
-              [{b:900,d:2000,x:-379,e:{x:7}}],
-              [{b:900,d:2000,x:-379,e:{x:7}}],
-              [{b:-1,d:1,o:-1,sX:2,sY:2},{b:0,d:900,x:-171,y:-341,o:1,sX:-2,sY:-2,e:{x:3,y:3,sX:3,sY:3}},{b:900,d:1600,x:-283,o:-1,e:{x:16}}]
-            ];
-
-            var jssor_1_options = {
-              $AutoPlay: 1,
-              $SlideDuration: 800,
-              $SlideEasing: $Jease$.$OutQuint,
-              $CaptionSliderOptions: {
-                $Class: $JssorCaptionSlideo$,
-                $Transitions: jssor_1_SlideoTransitions
-              },
-              $ArrowNavigatorOptions: {
-                $Class: $JssorArrowNavigator$
-              },
-              $BulletNavigatorOptions: {
-                $Class: $JssorBulletNavigator$
-              }
-            };
-
-            var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
-
-            /*#region responsive code begin*/
-
-            var MAX_WIDTH = 3000;
-
-            function ScaleSlider() {
-                var containerElement = jssor_1_slider.$Elmt.parentNode;
-                var containerWidth = containerElement.clientWidth;
-
-                if (containerWidth) {
-
-                    var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
-
-                    jssor_1_slider.$ScaleWidth(expectedWidth);
-                }
-                else {
-                    window.setTimeout(ScaleSlider, 30);
-                }
-            }
-
-            ScaleSlider();
-
-            $Jssor$.$AddEvent(window, "load", ScaleSlider);
-            $Jssor$.$AddEvent(window, "resize", ScaleSlider);
-            $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
-            /*#endregion responsive code end*/
-        };
-    </script>
+    
 
 </head>
 
@@ -99,11 +45,21 @@
                           
 </header>	
 
-<div id="filme_topo">
+<section id="filme_topo_container">
 	
-	<img src="img/capa_paginas.jpg" alt="img_topo">
+	<div id="filme_info">
+		
+		<div id='poster_filme'>
+		
+			<img src='img/posters/submersao.jpg'>
+		
+		</div>
+		
+		<div id="info"></div>
+		
+	</div>
 	
-</div>	
+</section>	
 
 
 <?php
@@ -125,17 +81,6 @@ $pgtitulo = $_GET["pgtitulo"];
 		?>
 	</div>
 
-
-<!--<div class="pg_titulo">
-	
-	//<?php
-//	$pgtitulo = $_GET["pgtitulo"];
-//	
-//	if ($pgtitulo == "$pgtitulo") { echo "<h1>$pgtitulo</h1>";}
-//	
-//	?>
-	
-</div>-->
 
 <div id="corpo_filme">
 
